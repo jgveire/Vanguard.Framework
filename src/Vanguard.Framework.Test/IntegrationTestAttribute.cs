da7b05323e2bcec618a3736330525eefac5a0340
Jorrit <jorrit@veire.com>
2017-09-11 21:02:55 +0200

    Added test category attributes.

---
+++ src/Vanguard.Framework.Test/IntegrationTestAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vanguard.Framework.Test
+{
+    /// <summary>
+    /// The integration test category attribute class.
+    /// </summary>
+    /// <seealso cref="Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryBaseAttribute" />
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class IntegrationTestAttribute : TestCategoryBaseAttribute
+    {
+        /// <inheritdoc />
+        public override IList<string> TestCategories { get; } = new List<string> { "Integration" };
+    }
+}
