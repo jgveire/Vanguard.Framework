eae2a67fa7447ece848ffd73e74384ec7514fc48
Jorrit <jorrit@veire.com>
2017-12-24 12:21:27 +0100

    Moved models to separate project.

---
+++ src/ExampleModels/CarModel.cs
@@ -0,0 +1,23 @@
+﻿namespace ExampleModels
+{
+    using System.ComponentModel.DataAnnotations;
+    using Vanguard.Framework.Core.Models;
+
+    public class CarModel : UniqueModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Brand { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Model { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LicensePlate { get; protected set; }
+
+        [Required]
+        public bool IsStolen { get; protected set; }
+    }
+}
---
+++ src/ExampleModels/GarageModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ExampleModels
+{
+    using System.ComponentModel.DataAnnotations;
+    using Vanguard.Framework.Core.Models;
+
+    public class GarageModel : UniqueModel
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Address { get; set; }
+    }
+}
