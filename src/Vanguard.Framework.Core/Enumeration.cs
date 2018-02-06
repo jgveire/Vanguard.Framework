7ce002a864163ed2b871e2d9f617d6a76b434747
Jorrit <jorrit@veire.com>
2018-02-06 21:29:43 +0100

    Add enumeration base class

---
+++ src/Vanguard.Framework.Core/Enumeration.cs
@@ -0,0 +1,158 @@
+﻿namespace Vanguard.Framework.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    /// <summary>
+    /// The enumeration base class for handling enums in an alternative way.
+    /// </summary>
+    /// <seealso cref="System.IComparable" />
+    public abstract class Enumeration : IComparable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Enumeration"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="displayName">The display name.</param>
+        protected Enumeration(int value, string displayName)
+        {
+            Value = value;
+            DisplayName = displayName;
+        }
+
+        /// <summary>
+        /// Gets the display name.
+        /// </summary>
+        /// <value>
+        /// The display name.
+        /// </value>
+        public string DisplayName { get; }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public int Value { get; }
+
+        /// <summary>
+        /// Gets the absolutes difference between two enumerations.
+        /// </summary>
+        /// <param name="firstValue">The first value.</param>
+        /// <param name="secondValue">The second value.</param>
+        /// <returns>The absolutes difference between two enumerations.</returns>
+        public static int AbsoluteDifference(Enumeration firstValue, Enumeration secondValue)
+        {
+            var absoluteDifference = Math.Abs(firstValue.Value - secondValue.Value);
+            return absoluteDifference;
+        }
+
+        /// <summary>
+        /// Gets the enumeration from the display name.
+        /// </summary>
+        /// <typeparam name="TEnumeration">The type of the enumeration.</typeparam>
+        /// <param name="displayName">The display name.</param>
+        /// <returns>
+        /// A enumeration.
+        /// </returns>
+        public static TEnumeration FromDisplayName<TEnumeration>(string displayName)
+            where TEnumeration : Enumeration
+        {
+            var matchingItem = Parse<TEnumeration, string>(displayName, "display name", item => item.DisplayName == displayName);
+            return matchingItem;
+        }
+
+        /// <summary>
+        /// Gets the enumeration from the value.
+        /// </summary>
+        /// <typeparam name="TEnumeration">The type of the enumeration.</typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns>A enumeration.</returns>
+        public static TEnumeration FromValue<TEnumeration>(int value)
+            where TEnumeration : Enumeration
+        {
+            var matchingItem = Parse<TEnumeration, int>(value, "value", item => item.Value == value);
+            return matchingItem;
+        }
+
+        /// <summary>
+        /// Gets all enumerations for the supplied type.
+        /// </summary>
+        /// <typeparam name="TEnumeration">The type of the enumeration.</typeparam>
+        /// <returns>A collection of enumerations.</returns>
+        public static IEnumerable<TEnumeration> GetAll<TEnumeration>()
+            where TEnumeration : Enumeration
+        {
+            var type = typeof(TEnumeration);
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+            foreach (var info in fields)
+            {
+                if (info.GetValue(null) is TEnumeration locatedValue)
+                {
+                    yield return locatedValue;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public int CompareTo(object other)
+        {
+            return Value.CompareTo(((Enumeration)other).Value);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            var otherValue = obj as Enumeration;
+            if (otherValue == null)
+            {
+                return false;
+            }
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Value.Equals(otherValue.Value);
+
+            return typeMatches && valueMatches;
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return DisplayName;
+        }
+
+        /// <summary>
+        /// Parses the specified value.
+        /// </summary>
+        /// <typeparam name="TEnumeration">The type of the enumeration.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="value">The value.</param>
+        /// <param name="description">The enumeration description.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>A enumeration.</returns>
+        /// <exception cref="ApplicationException">Thrown when the enumeration could not be found.</exception>
+        private static TEnumeration Parse<TEnumeration, TValue>(TValue value, string description, Func<TEnumeration, bool> predicate)
+            where TEnumeration : Enumeration
+        {
+            var matchingItem = GetAll<TEnumeration>().FirstOrDefault(predicate);
+
+            if (matchingItem == null)
+            {
+                var message = string.Format("'{0}' is not a valid {1} in {2}", value, description, typeof(TEnumeration));
+                throw new ApplicationException(message);
+            }
+
+            return matchingItem;
+        }
+    }
+}
---
+++ tests/Vanguard.Framework.Core.Tests/EnumerationTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Vanguard.Framework.Core.Tests
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class EnumerationTests
+    {
+        [TestMethod]
+        public void When_FromValue_is_called_then_the_correct_Truck_should_be_returned()
+        {
+            // Act
+            CarType result = Enumeration.FromValue<CarType>(1);
+
+            // Assert
+            result.Should().Be(CarType.Truck, because: "the car type truck has the value 1");
+        }
+
+        [TestMethod]
+        public void When_FromDisplayName_is_called_then_the_correct_Truck_should_be_returned()
+        {
+            // Act
+            CarType result = Enumeration.FromDisplayName<CarType>("Truck");
+
+            // Assert
+            result.Should().Be(CarType.Truck, because: "the car type truck has the display name 'Truck'");
+        }
+
+        private class CarType : Enumeration
+        {
+            public static readonly CarType Sedan = new CarType(0, "Sedan");
+
+            public static readonly CarType Truck = new CarType(1, "Truck");
+
+            public CarType(int value, string displayName)
+                : base(value, displayName)
+            {
+            }
+        }
+    }
+}
