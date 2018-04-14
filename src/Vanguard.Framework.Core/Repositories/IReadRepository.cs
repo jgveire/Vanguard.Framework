5a418f2e916118241c109ce26d8e192a359ef03d
Jorrit <jorrit@veire.com>
2018-04-14 14:07:45 +0200

    Fixed spelling errors.

--- src/Vanguard.Framework.Core/Repositories/IReadRepository.cs
+++ src/Vanguard.Framework.Core/Repositories/IReadRepository.cs
@@ -14,27 +14,27 @@
         where TEntity : class, IDataEntity
     {
         /// <summary>
-        /// Finds entities accoording to the supplied find criteria.
+        /// Finds entities according to the supplied find criteria.
         /// </summary>
         /// <param name="filterQuery">The find criteria.</param>
-        /// <param name="filter">An aditional filter to apply on the result query.</param>
+        /// <param name="filter">An additional filter to apply on the result query.</param>
         /// <returns>A collection of entities.</returns>
         IEnumerable<TEntity> Find(
             FilterQuery filterQuery,
             Expression<Func<TEntity, bool>> filter = null);
 
         /// <summary>
-        /// Finds entities accoording to the supplied find criteria asynchronously.
+        /// Finds entities according to the supplied find criteria asynchronously.
         /// </summary>
         /// <param name="filterQuery">The find criteria.</param>
-        /// <param name="filter">An aditional filter to apply on the result query.</param>
+        /// <param name="filter">An additional filter to apply on the result query.</param>
         /// <returns>A collection of entities.</returns>
         Task<IEnumerable<TEntity>> FindAsync(
             FilterQuery filterQuery,
             Expression<Func<TEntity, bool>> filter = null);
 
         /// <summary>
-        /// Gets the enties accoording to the specified filter.
+        /// Gets the entities according to the specified filter.
         /// </summary>
         /// <param name="filter">The filter predicate.</param>
         /// <param name="orderBy">The order by predicate.</param>
@@ -46,7 +46,7 @@
             params string[] includeProperties);
 
         /// <summary>
-        /// Gets the enties accoording to the specified filter asynchronously.
+        /// Gets the entities according to the specified filter asynchronously.
         /// </summary>
         /// <param name="filter">The filter predicate.</param>
         /// <param name="orderBy">The order by predicate.</param>
@@ -72,31 +72,31 @@
         Task<TEntity> GetByIdAsync(params object[] id);
 
         /// <summary>
-        /// Gets the number of items in the database accoording to the supplied find criteria.
+        /// Gets the number of items in the database according to the supplied find criteria.
         /// </summary>
         /// <param name="filterQuery">The find criteria.</param>
-        /// <returns>The total number of items in the database accoording to the supplied find criteria.</returns>
+        /// <returns>The total number of items in the database according to the supplied find criteria.</returns>
         int GetCount(FilterQuery filterQuery);
 
         /// <summary>
-        /// Gets the number of items in the database accoording to the supplied filter.
+        /// Gets the number of items in the database according to the supplied filter.
         /// </summary>
         /// <param name="filter">The filter predicate.</param>
-        /// <returns>The total number of items in the database accoording to the supplied filter.</returns>
+        /// <returns>The total number of items in the database according to the supplied filter.</returns>
         int GetCount(Expression<Func<TEntity, bool>> filter = null);
 
         /// <summary>
-        /// Gets the number of items in the database accoording to the supplied find criteria asynchronously.
+        /// Gets the number of items in the database according to the supplied find criteria asynchronously.
         /// </summary>
         /// <param name="filterQuery">The find criteria.</param>
-        /// <returns>The total number of items in the database accoording to the supplied find criteria.</returns>
+        /// <returns>The total number of items in the database according to the supplied find criteria.</returns>
         Task<int> GetCountAsync(FilterQuery filterQuery);
 
         /// <summary>
-        /// Gets the number of items in the database accoording to the supplied filter asynchronously.
+        /// Gets the number of items in the database according to the supplied filter asynchronously.
         /// </summary>
         /// <param name="filter">The filter predicate.</param>
-        /// <returns>The total number of items in the database accoording to the supplied filter.</returns>
+        /// <returns>The total number of items in the database according to the supplied filter.</returns>
         Task<int> GetCountAsync(Expression<Func<TEntity, bool>> filter = null);
     }
 }
