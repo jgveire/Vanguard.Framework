c5259e553405aca5e3952c648eceb202f6bad04c
Jorrit <jorrit@veire.com>
2018-06-11 21:35:29 +0200

    Added filter to filter query

---
+++ src/Vanguard.Framework.Core/Extensions/CollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Vanguard.Framework.Core.Extensions
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The collection extensions.
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Adds range of items to the collection.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the collection.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="items">The items to add.</param>
+        public static void AddRange<T>(this ICollection<T> source, IEnumerable<T> items)
+        {
+            Guard.ArgumentNotNull(source, nameof(source));
+            Guard.ArgumentNotNull(items, nameof(items));
+
+            foreach (var item in items)
+            {
+                source.Add(item);
+            }
+        }
+    }
+}
--- src/Vanguard.Framework.Core/FilterQuery.cs
+++ src/Vanguard.Framework.Core/FilterQuery.cs
@@ -7,21 +7,34 @@
     public class FilterQuery
     {
         /// <summary>
+        /// Gets or sets the filter to apply.
+        /// For example:
+        /// - id eq 1
+        /// - id eq 0F17B728-E080-4581-B192-DD475C638DCB
+        /// - name eq 'Bike'
+        /// </summary>
+        /// <value>
+        /// The filter to apply.
+        /// </value>
+        public string Filter { get; set; }
+
+        /// <summary>
+        /// Gets or sets a comma separated list of complex properties
+        /// that should be include in the result.
+        /// For example: category,orders
+        /// </summary>
+        /// <value>
+        /// A comma separated list of properties.
+        /// </value>
+        public string Include { get; set; }
+
+        /// <summary>
         /// Gets or sets the order by.
         /// </summary>
         /// <value>
         /// The order by.
         /// </value>
         public string OrderBy { get; set; }
-
-        /// <summary>
-        /// Gets or sets the sort order.
-        /// The default value is ascending.
-        /// </summary>
-        /// <value>
-        /// The sort order.
-        /// </value>
-        public SortOrder SortOrder { get; set; }
 
         /// <summary>
         /// Gets or sets the page.
@@ -56,5 +69,14 @@
         /// The fields that should be selected.
         /// </value>
         public string Select { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sort order.
+        /// The default value is ascending.
+        /// </summary>
+        /// <value>
+        /// The sort order.
+        /// </value>
+        public SortOrder SortOrder { get; set; }
     }
 }
