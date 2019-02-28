4e560b417ecf927cad8a1fa3a0527fb519c8da1c
Jorrit <jorrit@veire.com>
2019-02-28 12:08:36 +0100

    Added Content Disposition helper method.

--- src/Vanguard.Framework.Http/Extensions/HeaderDictionaryExtensions.cs
+++ src/Vanguard.Framework.Http/Extensions/HeaderDictionaryExtensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace Vanguard.Framework.Http.Extensions
 {
+    using System.Net.Mime;
     using Microsoft.AspNetCore.Http;
 
     /// <summary>
@@ -7,6 +8,23 @@
     /// </summary>
     public static class HeaderDictionaryExtensions
     {
+        /// <summary>
+        /// Adds a content disposition header to the header dictionary.
+        /// </summary>
+        /// <param name="source">The header  dictionary.</param>
+        /// <param name="fileName">Name of the file.</param>
+        /// <param name="displayInline">if set to <c>true</c> the file should be displayed in line.</param>
+        public static void AddContentDisposition(this IHeaderDictionary source, string fileName, bool displayInline = true)
+        {
+            Guard.ArgumentNotNull(source, nameof(source));
+            var contentDisposition = new ContentDisposition
+            {
+                FileName = fileName,
+                Inline = displayInline
+            };
+            source.Add("Content-Disposition", contentDisposition.ToString());
+        }
+
         /// <summary>
         /// Adds the total count to the header dictionary with the key X-Total-Count.
         /// </summary>
